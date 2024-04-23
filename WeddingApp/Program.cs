c14b475a4904f8110178daac0df69afc6bcf9549
Tyreyn <athosikish33@gmail.com>
2024-04-23 15:56:35 +0200

    authenticate from localstorage

--- WeddingApp/Program.cs
+++ WeddingApp/Program.cs
@@ -1,14 +1,12 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Components.Authorization;
 using WeddingApp.Components;
 using WeddingApp.Context;
 using WeddingApp.Controllers;
 using WeddingApp.Entities;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add secrets variables
-//var connectionStrings = builder.Configuration.GetSection("SQL");
 
 builder.Configuration.AddUserSecrets("ConnectionStringClass");
 
@@ -18,24 +16,24 @@ builder.Services
     .AddInteractiveServerComponents();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddSingleton<CustomAuthState>();
 builder.Services.AddSingleton<SqlServerDataAccess>();
 builder.Services.AddSingleton<SqlServerDataController>();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProviderController>();
 builder.Services.AddScoped<UserEntity>();
-builder.Services
-    .AddAuthentication(options =>
-    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options =>
-    {
-        options.Cookie.Name = "auth_token";
-        options.LoginPath = "/login";
-        options.Cookie.MaxAge = TimeSpan.FromDays(30);
-        options.SlidingExpiration = true;
-        options.Cookie.SameSite = SameSiteMode.Strict;
-        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-        options.AccessDeniedPath = "/Forbidden/";
-    });
-
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+        .AddCookie(options =>
+        {
+            options.Cookie.Name = "auth_token";
+            options.LoginPath = "/login";
+            options.Cookie.MaxAge = TimeSpan.FromDays(30);
+            options.SlidingExpiration = true;
+            options.Cookie.SameSite = SameSiteMode.Strict;
+            options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+            options.AccessDeniedPath = "/Forbidden/";
+            options.Cookie.HttpOnly = true;
+        });
 builder.Services.AddAuthorization();
 var app = builder.Build();
 app.UseCookiePolicy();
@@ -52,7 +50,7 @@ app.UseAuthentication();
 app.UseRouting();
 app.UseAntiforgery();
 app.UseAuthorization();
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
