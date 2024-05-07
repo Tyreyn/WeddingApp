9876d57a73ca23e603889da715cb1595f08ebd42
Tyreyn <athosikish33@gmail.com>
2024-05-07 17:13:13 +0200

    fix3

---
+++ TestsLibrary/Frontend/MainPage.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+
+namespace TestsLibrary.Frontend
+{
+    [Parallelizable(ParallelScope.Self)]
+    [TestFixture]
+    public class MainPage
+    {
+        [Test]
+        public async Task LoginWithCorrectCredientals()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false,
+            });
+            var context = await browser.NewContextAsync();
+            var page = await context.NewPageAsync();
+
+            await page.GotoAsync("http://3.71.218.200/");
+
+            await page.Locator("input[type=\"text\"]").First.FillAsync("Michał");
+
+            await page.Locator("input[type=\"text\"]").Nth(1).FillAsync("123");
+
+            await page.GetByRole(AriaRole.Button, new() { NameString = "Zaloguj" }).ClickAsync();
+
+            await page.Locator("label:has-text(\"Dodaj zdjęcie\")").IsVisibleAsync();
+        }
+    }
+}
