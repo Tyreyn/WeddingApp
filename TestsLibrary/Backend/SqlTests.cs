121682ca13622aa38e051c0c5cad19250059d049
Tyreyn <athosikish33@gmail.com>
2024-05-07 14:07:25 +0200

    fix

--- TestsLibrary/Backend/SqlTests.cs
+++ TestsLibrary/Backend/SqlTests.cs
@@ -1,45 +1,32 @@
-using Microsoft.Extensions.Configuration;
-using System.Net.NetworkInformation;
-using WeddingApp.Context;
-using Bunit;
-
-using Moq;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Mvc;
-using WeddingApp.Entities;
-using AngleSharp;
-using Xunit.Abstractions;
-using Microsoft.Data.SqlClient;
-using WeddingApp.Helpers.SqlCommands;
-using AngleSharp.Common;
-using TestsLibrary.Helpers.Entity;
+﻿using Microsoft.Extensions.Configuration;
 using Dapper;
+using System.Collections.Generic;
+using System;
+using WeddingApp.Helpers.SqlCommands;
+using TestsLibrary.Helpers.Entity;
+
 namespace TestsLibrary.Backend
 {
-    public class SqlTests : IDisposable
+    [TestFixture]
+    public class SqlTests
     {
         /// <summary>
         /// SQL server data access.
         /// </summary>
-        private readonly SqlServerDataAccess sqlServerDataAccess;
-
-        /// <summary>
-        /// The test output helper interface.
-        /// </summary>
-        private readonly ITestOutputHelper output;
+        private SqlServerDataAccess sqlServerDataAccess;
 
         /// <summary>
         /// The test user object.
         /// </summary>
         private UserEntity testUser;
 
-        private bool disposedValue;
 
         private int maxUsersID;
 
-        public SqlTests(ITestOutputHelper output)
+
+        [SetUp]
+        public void Setup()
         {
-            this.output = output;
             var configuration = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json")
                 .AddUserSecrets<ConnectionStringClass>()
@@ -53,21 +40,16 @@ namespace TestsLibrary.Backend
             this.maxUsersID = tmp[0].MAX;
         }
 
-        [Fact]
+        [Test]
         public void CheckSqlServerConnection()
         {
-            Assert.True(sqlServerDataAccess.CheckConnectionStatusAndSetProperConnectionString().Result);
+            Assert.That(sqlServerDataAccess.CheckConnectionStatusAndSetProperConnectionString().Result);
         }
 
-        [Fact]
-        public void CheckProperUserInsert()
+        [TestCase(true)]
+        public void CheckProperUserInsert(bool expectedResult = true)
         {
             List<UserEntity> tmpUserEntity = sqlServerDataAccess.ExecuteStoredProcedures<UserEntity>(SqlCommands.ShowAllUsers).Result;
-            this.output.WriteLine(
-                "Before: {0}",
-                string.Join(
-                    $"{Environment.NewLine}",
-                    tmpUserEntity.Select(x => x.UserPhone)));
             bool result = sqlServerDataAccess.ExecuteStoredProcedures<UserEntity>(SqlCommands.InsertNewUser, new DynamicParameters(
                         new
                         {
@@ -75,45 +57,30 @@ namespace TestsLibrary.Backend
                             Name = this.testUser.UserName
                         })).IsCompletedSuccessfully;
             tmpUserEntity = sqlServerDataAccess.ExecuteStoredProcedures<UserEntity>(SqlCommands.ShowAllUsers).Result;
-            this.output.WriteLine(
-                "Before: {0}",
-                string.Join(
-                    $"{Environment.NewLine}",
-                    tmpUserEntity.Select(x => x.UserPhone)));
-            Assert.True(result);
+            Assert.That(result == expectedResult);
         }
 
-        [Fact]
+        [Test]
         public void CheckIfTwoUsersWithSamePhoneNumberCanBeAdded()
         {
-            this.output.WriteLine("First iteration");
             this.CheckProperUserInsert();
-            this.output.WriteLine("Second iteration");
             try
             {
-                this.CheckProperUserInsert();
+                this.CheckProperUserInsert(expectedResult: false);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                Assert.True(true, "Database didn't insert same user");
+                Assert.That(true, "Database didn't insert same user");
             }
         }
 
-        protected virtual void Dispose(bool disposing)
+        [TearDown]
+        public void TearDown()
         {
-            if (disposing)
-            {
-                this.output.WriteLine("Cleaning");
-                sqlServerDataAccess.ExecuteStoredProcedures<UserEntity>(SqlCommands.DeleteUserById, new DynamicParameters(new { ID = this.maxUsersID + 1 }));
-                this.output.WriteLine("Reseed to {0}", this.maxUsersID);
-                sqlServerDataAccess.ExecuteStoredProcedures<string>(SqlCommands.Reseed, new DynamicParameters(new { LASTID = this.maxUsersID }));
-            }
+            sqlServerDataAccess.ExecuteStoredProcedures<UserEntity>(SqlCommands.DeleteUserById, new DynamicParameters(new { ID = this.maxUsersID + 1 }));
+            sqlServerDataAccess.ExecuteStoredProcedures<string>(SqlCommands.Reseed, new DynamicParameters(new { LASTID = this.maxUsersID }));
+
         }
 
-        public void Dispose()
-        {
-            Dispose(disposing: true);
-            GC.SuppressFinalize(this);
-        }
     }
 }
